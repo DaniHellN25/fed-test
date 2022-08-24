95a390e373c7fee5169c70e5bb68bd36293aa5cc
Pedro Daniel Chi Novelo <danihellnovelo18@gmail.com>
2022-08-23 22:51:41 -0500

    refactor: more readable and reusable code

--- src/js/main.js
+++ src/js/main.js
@@ -35,3 +35,23 @@ var reportsWidget = {
 };
 
 reportsWidget.init();
+
+
+//Possible way to refactor 
+//This way we keep the code more maintainable, readable and reusable
+// var reportsWidget = {
+//     fileName: "main",
+//     data: reportData,
+//     containerSelector: ".reports",
+//   };
+  
+//   function renderMustacheTemplate({ fileName, data, containerSelector }) {
+//     fetch(`./templates/${fileName}.mustache`)
+//       .then((response) => response.text())
+//       .then((template) => {
+//         var rendered = Mustache.render(template, data || []);
+//         document.querySelector(containerSelector).innerHTML = rendered;
+//       });
+//   }
+
+// renderMustacheTemplate(reportsWidget);
